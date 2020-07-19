e20df76b36cbbe590259e74d6a9f587f401565a2
Davidn75 <yyr18fzu@uea.ac.uk>
2020-07-19 00:41:03 +0000

    >Tank-O-Mania.cpp + Added white box to test collision detection + Cleaned class up a bit + Implemented some sort of tempremental collision response

--- Tank-O-Mania.cpp
+++ Tank-O-Mania.cpp
@@ -21,27 +21,34 @@ bool LeftPressed = false;
 int screenWidth = 480, screenHeight = 480;
 bool keys[256];
 float spin = 0;
-float speed = 0;
+
+
+float speed = 0.05f;                                    // speed used as variable for tank1 speed
+font_data our_font;                                 // font used to print message on display
+float timer;                                        // timer used as game timer to calculate maths with time
+
+float tank1XMovement = 0;                       // used to track transformation for tank X movement
+float tank1YMovement = 0;	                        // used to track transformation for tank Y movement  
+float tank1Angle = 0.0;
+float tank1Velocity = 0;
+
+float tank1Matrix[16];
+float squareMatrix[16];
+
+float squareXTransform = 0;
+float squareYTransform = 0; // transform square x and y by arrrow key transformation
+
+
+
 POINT p;
 
 
-OBB square, triangle;
+OBB tank1OBB;
+OBB squareOBB;
 
 
 
 
-/**************************** variables for font ****************************/
-font_data our_font;
-
-
-
-/**************************** variables for timer ****************************/
-float number;
-
-
-
-/**************************** variables for transformation matrix ****************************/
-float squareTransformationMatrix[16];
 
 
 
@@ -73,29 +80,26 @@ float blue = 0.0;
 
 
 
-/**************************** variables for arrow key transformation ****************************/
-float tank1XMovement = -1200;
-float tank1YMovement =     0;	// transform triangle x and y by arrrow key transformation
-float tank1Angle = 0.0;
-float tank1Velocity = 0;
-
-float squareXTransform = 0;
-float squareYTransform = 0; // transform square x and y by arrrow key transformation
 
 
 
-/**************************** functions ****************************/
+
+/**************************** function declarations ****************************/
 void drawCircle(float x, float y, float radius, float angle); //prototype the draw function
 void changeLineColor();
 void spiningSquareCorner();
 void circleCollison();
 void shapesCollison();
 void printFunctions();
-void AABBCollision();
+void drawEasyMap();
+void drawMediumMap();
+void drawHardMap();
 
-void drawDots();
+void drawTank1Sprite();
+void moveTank1Sprite();
+
 void moveCamera();
-bool outsideObject(Vertex P, Vertex V[], int n);
+bool outsideObject(Point P, Point V[], int n);
 
 /**************************** OPENGL FUNCTION PROTOTYPES ****************************/
 void display();				//draws everything to the screen
@@ -164,6 +168,30 @@ void init()
 	tank1Body   = loadPNG(png3);
 	tank1Barrel = loadPNG(png4);
 
+	tank1OBB.vertOriginal[0].x = -18;
+	tank1OBB.vertOriginal[0].y = -26;
+
+	tank1OBB.vertOriginal[1].x = -18;
+	tank1OBB.vertOriginal[1].y = 26;
+
+	tank1OBB.vertOriginal[2].x = 18;
+	tank1OBB.vertOriginal[2].y = 26;
+
+	tank1OBB.vertOriginal[3].x = 18;
+	tank1OBB.vertOriginal[3].y = -26;
+
+	squareOBB.vertOriginal[0].x = 100;
+	squareOBB.vertOriginal[0].y = -900;
+
+	squareOBB.vertOriginal[1].x = 100;
+	squareOBB.vertOriginal[1].y = 600;
+
+	squareOBB.vertOriginal[2].x = 200;
+	squareOBB.vertOriginal[2].y = 600;
+
+	squareOBB.vertOriginal[3].x = 200;
+	squareOBB.vertOriginal[3].y = -900;
+
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -177,19 +205,12 @@ void display()
 
 		glClear(GL_COLOR_BUFFER_BIT);
 
-	glMatrixMode(GL_MODELVIEW);
+		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 
-		// white axis lines
-		//glLineWidth(1.5);
-		//glColor3f(1.0, 1.0, 1.0);
-		//	glBegin(GL_LINES);
-		//		glVertex2f(-3000, 3000);
-		//		glVertex2f(3000, 3000);
-		//		glVertex2f(3000, 3000);
-		//		glVertex2f(3000, -3000);
-		//	glEnd();
+	
 		
+		// glPushMatrix - draws the whole map size 6000x6000 and texture maps green to it
 		glPushMatrix();
 			glEnable(GL_TEXTURE_2D);
 			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
@@ -204,61 +225,22 @@ void display()
 		glPopMatrix();
 
 
-		glPushMatrix();
-		glEnable(GL_TEXTURE_2D);
-		    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-			glBindTexture(GL_TEXTURE_2D, tank1Tracks);
-			glTranslatef(tank1XMovement, tank1YMovement, 0.0);
-			glRotatef(tank1Angle, 0, 0, 1);
-			glColor3f(1,1,1);
-			glBegin(GL_POLYGON);
-			glTexCoord2f(0,0);	glVertex2f(-36, -52);
-			glTexCoord2f(0,1);	glVertex2f(-36,  52);
-			glTexCoord2f(1,1);	glVertex2f( 36,  52);
-			glTexCoord2f(1,0);	glVertex2f( 36, -52);
-			glEnd();
-			glDisable(GL_TEXTURE_2D);
-		glPopMatrix();
 
 		glPushMatrix();
-			glEnable(GL_TEXTURE_2D);
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-			glBindTexture(GL_TEXTURE_2D, tank1Body);
-			glTranslatef(tank1XMovement, tank1YMovement, 0.0);
-			glRotatef(tank1Angle, 0, 0, 1);
-			glColor3f(1, 1, 1);
-			glBegin(GL_POLYGON);
-				glTexCoord2f(0, 0);	glVertex2f(-33, -41);
-				glTexCoord2f(0, 1);	glVertex2f(-33, 41);
-				glTexCoord2f(1, 1);	glVertex2f(33, 41);
-				glTexCoord2f(1, 0);	glVertex2f(33, -41);
+		glColor3f(1.0, 1.0, 1.0);
+			glBegin(GL_LINE_STRIP);
+				glVertex2f(-25, -25);
+				glVertex2f(-25, 25);
+				glVertex2f(25, 25);
+				glVertex2f(25, -25);
 			glEnd();
-			glDisable(GL_TEXTURE_2D);
-		glPopMatrix();
-
-		glPushMatrix();
-			glEnable(GL_TEXTURE_2D);
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-			glBindTexture(GL_TEXTURE_2D, tank1Barrel);
-			glTranslatef(tank1XMovement, tank1YMovement, 0.0);
-			glRotatef(tank1Angle, 0, 0, 1);
-			glColor3f(1, 1, 1);
-			glBegin(GL_POLYGON);
-				glTexCoord2f(0, 0);	glVertex2f(-8,   0);
-				glTexCoord2f(0, 1);	glVertex2f(-8,  80);
-				glTexCoord2f(1, 1);	glVertex2f( 8,  80);
-				glTexCoord2f(1, 0);	glVertex2f( 8,   0);
-			glEnd();
-			glDisable(GL_TEXTURE_2D);
-		glPopMatrix();
+			glGetFloatv(GL_MODELVIEW_MATRIX, squareMatrix);
+			glPopMatrix();
 
 
 
 
 
-		//update the position of the user controlled object.
-		tank1XMovement += tank1Velocity * cosf((90 + tank1Angle) * (PI / 180.0f));
-		tank1YMovement += tank1Velocity * sinf((90 + tank1Angle) * (PI / 180.0f));
 		
 		int i = 200;
 		/*
@@ -274,7 +256,7 @@ void display()
 			glVertex2f(-50 + i, 0);
 		glEnd();
 		glPopMatrix();
-		*/
+		
 
 		glPushMatrix();
 		glColor3f(1, 1, 1);
@@ -295,12 +277,12 @@ void display()
 			glVertex2f(-1500, 1500);
 		glEnd();
 		glPopMatrix();
-
+		*/
 
 		//Vertex v2[] = { {-50 + i,0}, {-50 + i, 1500}, {500 + i,2000}, {1000 + i, 2100}, {1300 + i,1300}, {1300 + i, 0}, {0 + i, 0} };
-		Vertex v2[] = { {-1000, -1000}, {-1000, 1000}, {1000, 1000}, {1000, -1000} };
-		Vertex p2 = { tank1XMovement, tank1YMovement };
-	    int n2 = sizeof(v2) / sizeof(v2[0]);
+		//Vertex v2[] = { {-1000, -1000}, {-1000, 1000}, {1000, 1000}, {1000, -1000} };
+		//Vertex p2 = { tank1XMovement, tank1YMovement };
+	    //int n2 = sizeof(v2) / sizeof(v2[0]);
 
 		/*
 		glPushMatrix();
@@ -318,120 +300,29 @@ void display()
 		*/
 
 		//Vertex v[] = { {100 + i,100}, {100 + i,1100}, {600 + i,1600}, {900 + i, 1900}, {1100 + i,1100}, {1100 + i,100}, {100 + i,100} };
-		Vertex v[] = { {-1500, -1500}, {-1500, 1500}, {1500, 1500}, {1500, -1500} };
-		Vertex p = {tank1XMovement, tank1YMovement};
-		int n = sizeof(v) / sizeof(v[0]);
+		//Vertex v[] = { {-1500, -1500}, {-1500, 1500}, {1500, 1500}, {1500, -1500} };
+		//Vertex p = {tank1XMovement, tank1YMovement};
+		//int n = sizeof(v) / sizeof(v[0]);
 
-		if (!outsideObject(p, v, n) && outsideObject(p2, v2, n2))
-		{
-			speed = 1.5f;
-			print(our_font, 20, 35, "Inside");
-		}
-		else
-		{
+		//if (!outsideObject(p, v, n) && outsideObject(p2, v2, n2))
+		//{
+			//speed = 1.5f;
+			//print(our_font, 20, 35, "Inside");
+		//}
+		//else
+		//{
 
-			speed = 0.5f;
-			print(our_font, 20, 35, "Outside");
-		}
+			//speed = 1.5f;
+			//print(our_font, 20, 35, "Outside");
+		//}
 
-		/*
-		// hard map		
-		glPushMatrix();
-			glColor3f(1,1,1);
-			glBegin(GL_LINE_STRIP);
-				glVertex2f(0, 0);
-				glVertex2f(0, 1200);
-				glVertex2f(1000, 1200);
-				glVertex2f(1000, 0);
-				glVertex2f(1900, 0);
-				glVertex2f(1900, 2500);
-				glVertex2f(2500, 2500);
-				glVertex2f(2500, -2500);
-				glVertex2f(-2500, -2500);
-				glVertex2f(-2500, -1800);
-				glVertex2f(-1400, -1800);
-				glVertex2f(-1400, -1100);
-				glVertex2f(-2500, -1100);
-				glVertex2f(-2500, 2500);
-				glVertex2f(200, 2500);
-				glVertex2f(200, 2000);
-				glVertex2f(-1800, 2000);
-				glVertex2f(-1800, 500);
-				glVertex2f(-1200, 500);
-				glVertex2f(-1200, -400);
-				glVertex2f(-600, -400);
-				glVertex2f(-600, -1500);
-				glVertex2f(900, -1500);
-				glVertex2f(900, -800);
-				glVertex2f(0, -800);
-				glVertex2f(0, 0);
-			glEnd();
-		glPopMatrix();
-		*/
 		
 		//drawCircle(0, 0, 2000, 179);
 
-		// medium map
-		/*
-		glPushMatrix();
-			glColor3f(1,1,1);
-			glBegin(GL_LINE_STRIP);
-				glVertex2f(0, 0);
-				glVertex2f(0, 2500);
-				glVertex2f(-2500, 2500);
-				glVertex2f(-2500, 1900);
-				glVertex2f(-1500, 1900);
-				glVertex2f(-1500, -1900);
-				glVertex2f(-2200, -1900);
-				glVertex2f(-2200, -2500);
-				glVertex2f(2500, -2500);
-				glVertex2f(2500, 2100);
-				glVertex2f(1800, 2100);
-				glVertex2f(1800, -1000);
-				glVertex2f(1000, -1000);
-				glVertex2f(1000, -500);
-				glVertex2f(0, -500);
-				glVertex2f(0, 0);
-			glEnd();
-		glPopMatrix();
-		*/
+
 		
-		// easy map
-		/*
-		glPushMatrix();
-		glColor3f(1, 1, 1);
-			glBegin(GL_LINE_STRIP);
-				glVertex2f(0, 0);
-				glVertex2f(0, 500);
-				for (float i = 0; i < 180; i += 5)
-				{
-					float xcoord = -1000 + 1000 * cosf(i * (PI / 180.0f));
-					float ycoord = 500 + 1000 * sinf(i * (PI / 180.0f));
-					glVertex2f(xcoord, ycoord);
-				}
-				glVertex2f(-2000, 500);
-				glVertex2f(-2000, -1800);
-				for (float i = 0; i < 90; i += 5)
-				{
-					float xcoord = -1800 - 200 * cosf(i * (PI / 180.0f));
-					float ycoord = -1800 - 200 * sinf(i * (PI / 180.0f));
-					glVertex2f(xcoord, ycoord);
-				}
-				glVertex2f(2000, -2000);
-				glVertex2f(2000, 1800);
-				for (float i = 0; i < 90; i += 5)
-				{
-					float xcoord = 1800 + 200 * cosf(i * (PI / 180.0f));
-					float ycoord = 1800 + 200 * sinf(i * (PI / 180.0f));
-					glVertex2f(xcoord, ycoord);
-				}
-				glVertex2f(1000, 2000);
-				glVertex2f(1000, -1000);
-				glVertex2f(0, -1000);
-				glVertex2f(0, 0);
-			glEnd();
-		glPopMatrix();
-		*/
+	
+		
 		
 
 		/*
@@ -471,25 +362,41 @@ void display()
 
 	glFlush();
 
-	square.transformPoints(squareTransformationMatrix);
+	tank1OBB.transformPoints(tank1Matrix);
+	squareOBB.transformPoints(squareMatrix);
 
+	float tempX = 0.0f;
 
-	if (square.SAT2D(triangle))
-		glColor3f(1.0, 0.0, 1.0);
-	else
-		glColor3f(0.0, 0.0, 1.0);
+	if (tank1OBB.SAT2D(squareOBB))
+	{
+		print(our_font, 20, 95, "COllision!");
 
-	square.drawOBB();
+		tank1Angle = tank1Angle + 180;
+ 
+		for(int i = 0; i < 100 ; i++)
+		{
+			tank1XMovement += tank1Velocity * cosf((90 + tank1Angle) * (PI / 180.0f));
+			tank1YMovement += tank1Velocity * sinf((90 + tank1Angle) * (PI / 180.0f));
+		} 
+		tank1Angle = tank1Angle + 180;	
+	}
 
-	triangle.drawOBB();
+	
+    squareOBB.drawOBB();
+	tank1OBB.drawOBB();
+	print(our_font, 20, 95, "temp: %7.2f", tempX);
 
 
 	/**************************** methods ****************************/
 	printFunctions();
 	shapesCollison();
-	AABBCollision();
+	drawEasyMap();
+	//drawMediumMap();
+	//drawHardMap();
+
+	drawTank1Sprite();
+    moveTank1Sprite();
 	
-	//drawDots();
     moveCamera();
 
 
@@ -498,9 +405,177 @@ void display()
 		spin = 0;
 }
 
+void drawHardMap()
+{
+	// hard map
+	glPushMatrix();
+		glColor3f(1,1,1);
+		glBegin(GL_LINE_STRIP);
+			glVertex2f(0, 0);
+			glVertex2f(0, 1200);
+			glVertex2f(1000, 1200);
+			glVertex2f(1000, 0);
+			glVertex2f(1900, 0);
+			glVertex2f(1900, 2500);
+			glVertex2f(2500, 2500);
+			glVertex2f(2500, -2500);
+			glVertex2f(-2500, -2500);
+			glVertex2f(-2500, -1800);
+			glVertex2f(-1400, -1800);
+			glVertex2f(-1400, -1100);
+			glVertex2f(-2500, -1100);
+			glVertex2f(-2500, 2500);
+			glVertex2f(200, 2500);
+			glVertex2f(200, 2000);
+			glVertex2f(-1800, 2000);
+			glVertex2f(-1800, 500);
+			glVertex2f(-1200, 500);
+			glVertex2f(-1200, -400);
+			glVertex2f(-600, -400);
+			glVertex2f(-600, -1500);
+			glVertex2f(900, -1500);
+			glVertex2f(900, -800);
+			glVertex2f(0, -800);
+			glVertex2f(0, 0);
+		glEnd();
+	glPopMatrix();
+}
 
+void drawMediumMap()
+{
+	// medium map
+	glPushMatrix();
+		glColor3f(1,1,1);
+		glBegin(GL_LINE_STRIP);
+			glVertex2f(0, 0);
+			glVertex2f(0, 2500);
+			glVertex2f(-2500, 2500);
+			glVertex2f(-2500, 1900);
+			glVertex2f(-1500, 1900);
+			glVertex2f(-1500, -1900);
+			glVertex2f(-2200, -1900);
+			glVertex2f(-2200, -2500);
+			glVertex2f(2500, -2500);
+			glVertex2f(2500, 2100);
+			glVertex2f(1800, 2100);
+			glVertex2f(1800, -1000);
+			glVertex2f(1000, -1000);
+			glVertex2f(1000, -500);
+			glVertex2f(0, -500);
+			glVertex2f(0, 0);
+		glEnd();
+	glPopMatrix();	
+}
 
-bool outsideObject(Vertex P, Vertex V[], int n)
+void drawEasyMap()
+{
+	glPushMatrix();
+		glColor3f(1,1,1);
+		glBegin(GL_POLYGON);
+			glVertex2f(100, -900);
+			glVertex2f(100, 600);
+			glVertex2f(200, 600);
+			glVertex2f(200, -900);
+		glEnd();
+	glPopMatrix();
+
+	// easy map
+	glPushMatrix();
+	glColor3f(1, 0, 0);
+		glBegin(GL_LINE_STRIP);
+			glVertex2f(0, 0);
+			glVertex2f(0, 500);
+			glVertex2f(-2000, 500);
+			glVertex2f(-2000, -2000);
+			glVertex2f(2000, -2000);
+			glVertex2f(2000, 2000);
+			glVertex2f(1000, 2000);
+			glVertex2f(1000, -1000);
+			glVertex2f(0, -1000);
+			glVertex2f(0, 0);
+		glEnd();
+	glPopMatrix();
+	
+	glPushMatrix();
+	glColor3f(1, 0, 0);
+		glBegin(GL_LINE_STRIP);
+			glVertex2f(100, 0);
+			glVertex2f(100, 600);
+			glVertex2f(-2100, 600);
+			glVertex2f(-2100, -2100);
+			glVertex2f(2100, -2100);
+			glVertex2f(2100, 2100);
+			glVertex2f(900, 2100);
+			glVertex2f(900, -900);
+			glVertex2f(100, -900);
+			glVertex2f(100, 0);
+		glEnd();
+	glPopMatrix();
+}
+
+void moveTank1Sprite()
+{
+	//update the position of the user controlled object.
+	tank1XMovement += tank1Velocity * cosf((90 + tank1Angle) * (PI / 180.0f));
+	tank1YMovement += tank1Velocity * sinf((90 + tank1Angle) * (PI / 180.0f));
+}
+
+void drawTank1Sprite()
+{
+	// glPushMatrix - draws the tank tracks and applies movement to tracks
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glBindTexture(GL_TEXTURE_2D, tank1Tracks);
+	glTranslatef(tank1XMovement, tank1YMovement, 0.0);
+	glRotatef(tank1Angle, 0, 0, 1);
+	glGetFloatv(GL_MODELVIEW_MATRIX, tank1Matrix);
+	glColor3f(1, 1, 1);
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0, 0);	glVertex2f(-18, -26);
+	glTexCoord2f(0, 1);	glVertex2f(-18, 26);
+	glTexCoord2f(1, 1);	glVertex2f(18, 26);
+	glTexCoord2f(1, 0);	glVertex2f(18, -26);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+
+	// glPushMatrix - draws the tank body and applies movement to body
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glBindTexture(GL_TEXTURE_2D, tank1Body);
+	glTranslatef(tank1XMovement, tank1YMovement, 0.0);
+	glRotatef(tank1Angle, 0, 0, 1);
+	glColor3f(1, 1, 1);
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0, 0);	glVertex2f(-15, -20);
+	glTexCoord2f(0, 1);	glVertex2f(-15, 20);
+	glTexCoord2f(1, 1);	glVertex2f(15, 20);
+	glTexCoord2f(1, 0);	glVertex2f(15, -20);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+
+	// glPushMatrix - draws the tank barrel and applies movement to barrel
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glBindTexture(GL_TEXTURE_2D, tank1Barrel);
+	glTranslatef(tank1XMovement, tank1YMovement, 0.0);
+	glRotatef(tank1Angle, 0, 0, 1);
+	glColor3f(1, 1, 1);
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0, 0);	glVertex2f(-4, 0);
+	glTexCoord2f(0, 1);	glVertex2f(-4, 40);
+	glTexCoord2f(1, 1);	glVertex2f(4, 40);
+	glTexCoord2f(1, 0);	glVertex2f(4, 0);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+}
+
+bool outsideObject(Point P, Point V[], int n)
 {
 	int cn = 0; // the crossing number counter
 	// loop through all edges of the polygon
@@ -522,62 +597,17 @@ void moveCamera()
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-		gluOrtho2D((tank1XMovement - screenWidth / 0.5), (tank1XMovement + screenWidth / 0.5), (tank1YMovement - screenHeight / 0.5), (tank1YMovement + screenHeight / 0.5));
+		gluOrtho2D((tank1XMovement - screenWidth / 2), (tank1XMovement + screenWidth / 2), (tank1YMovement - screenHeight / 2), (tank1YMovement + screenHeight / 2));
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
 
-void drawDots()
-{
-	float rgb = 0.0;
-
-	for (int i = 20; i < 500; i += 30)
-	{
-		glPushMatrix();
-			glColor3f(1.0, rgb, rgb);
-			glPointSize(5.0);
-			glBegin(GL_POINTS);
-				glVertex2f(20, i);
-			glEnd();
-		glPopMatrix();
-
-		rgb += 0.03;
-	}
-}
-
 void printFunctions()
-{
-	print(our_font, 20, 95, "tank1XMovement: %7.2f", tank1XMovement);
-	print(our_font, 20, 65, "tank1YMovement: %7.2f", tank1YMovement);
+{	
+	print(our_font, 0, 65, "%7.2f", tank1Velocity);
+	//print(our_font, 20, 65, "tank1YMovement: %7.2f", tank1YMovement);
 	//print(our_font, 20, 35, "squareMaxX: %7.2f", squareMaxX);
-	print(our_font, 20, 5,  "speed: %7.2f", speed);
-}
-
-void AABBCollision()
-{
-	square.vertOriginal[0].x = -4;
-	square.vertOriginal[0].y = -4;
-
-	square.vertOriginal[1].x = -4;
-	square.vertOriginal[1].y = 4;
-
-	square.vertOriginal[2].x = 4;
-	square.vertOriginal[2].y = 4;
-
-	square.vertOriginal[3].x = 4;
-	square.vertOriginal[3].y = -4;
-
-	triangle.vertOriginal[0].x = -4;
-	triangle.vertOriginal[0].y = -4;
-
-	triangle.vertOriginal[1].x = 4;
-	triangle.vertOriginal[1].y = -4;
-
-	triangle.vertOriginal[2].x = 0;
-	triangle.vertOriginal[2].y = 5.65f;
-
-	triangle.vertOriginal[3].x = 0;
-	triangle.vertOriginal[3].y = 5.65f;
+	//print(our_font, 20, 5,  "speed: %7.2f", speed);
 }
 
 void shapesCollison()
@@ -661,9 +691,9 @@ void changeLineColor()
 	//glEnd();
 	//end of code for drawing a line...
 
-	number = (((squareXTransform - tank1XMovement) * (squareXTransform - tank1XMovement)) + (squareYTransform - (tank1YMovement + 5.65f)) * (squareYTransform - (tank1YMovement + 5.65f)));
+	timer = (((squareXTransform - tank1XMovement) * (squareXTransform - tank1XMovement)) + (squareYTransform - (tank1YMovement + 5.65f)) * (squareYTransform - (tank1YMovement + 5.65f)));
 
-	if (number < 30 || number < -30)
+	if (timer < 30 || timer < -30)
 	{
 		green = 0.0;
 		red = 1.0;
@@ -698,7 +728,6 @@ void reshape(int width, int height)		// Resize the OpenGL window
 	glLoadIdentity();									// Reset The Projection Matrix
 
 	//gluOrtho2D(-4000, 4000, -4000, 4000);             // View whole map 
-	//gluOrtho2D(-300, 300, -300, 300);
 
 	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
 	glLoadIdentity();									// Reset The Modelview Matrix
@@ -708,11 +737,11 @@ void processKeys()
 {
 	if (keys[VK_LEFT])
 	{
-		tank1Angle += 0.2;
+		tank1Angle += 0.4;
 	}
 	if (keys[VK_RIGHT])
 	{
-		tank1Angle -= 0.2;
+		tank1Angle -= 0.4;
 	}
 	if (keys[VK_UP])
 	{
@@ -726,6 +755,31 @@ void processKeys()
 		tank1Velocity = -speed;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
